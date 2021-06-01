47b407d92bbd48b33d6508f42edf1559e5221e7b
mdelgado509 <mdelgado509@gmail.com>
2021-06-01 14:54:34 -0400

    Add destroy profile end point and test

--- app/routes/profile_routes.js
+++ app/routes/profile_routes.js
@@ -8,13 +8,13 @@ const Profile = require('../models/profile')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
-// const customErrors = require('../../lib/custom_errors')
+const customErrors = require('../../lib/custom_errors')
 
 // we'll use this function to send 404 when non-existant document is requested
-// const handle404 = customErrors.handle404
+const handle404 = customErrors.handle404
 // we'll use this function to send 401 when a user tries to modify a resource
 // that's owned by someone else
-// const requireOwnership = customErrors.requireOwnership
+const requireOwnership = customErrors.requireOwnership
 
 // this is middleware that will remove blank fields from `req.body`, e.g.
 // { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
@@ -40,6 +40,26 @@ router.post('/profiles', requireToken, (req, res, next) => {
       res.status(201).json({ profile: profile.toObject() })
     })
     // if an error occurs, pass to error handler
+    .catch(next)
+})
+
+// DESTROY
+// DELETE /profiles/60b67f2ad27b9da0c07e7008
+router.delete('/profiles/:id', requireToken, (req, res, next) => {
+  // Find by id
+  Profile.findById(req.params.id)
+    // handle404 if not found
+    .then(handle404)
+    // if found
+    .then(profile => {
+      // throw error if current user doesn't own the `profile`
+      requireOwnership(req, profile)
+      // delete `profile` if error didn't throw
+      profile.deleteOne()
+    })
+    // send back 204 no content if the deletion succeeded
+    .then(() => res.sendStatus(204))
+    // if error occurs, pass it to the handler
     .catch(next)
 })
 
